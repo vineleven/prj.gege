a4bfdf92408ed2cac7b6f2cd0edd68b396b72a06
vineleven <istill.so@qq.com>
2016-09-20 00:07:36 +0800

    test

---
+++ Assets/Scripts/Module/MgrScene.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MgrScene : MonoBehaviour {
+
+	// Use this for initialization
+
+
+	public static Camera uiCamera;
+
+
+
+	void Awake(){
+		uiCamera = GameObject.FindWithTag ("UiCamera").GetComponent<Camera>();
+	}
+
+
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- Assets/Scripts/Ui/JoyStick.cs
+++ Assets/Scripts/Ui/JoyStick.cs
@@ -7,35 +7,45 @@ public class JoyStick : MonoBehaviour {
 
 	// Use this for initialization
 
-	Transform circle;
-
-
 	Vector3 originPos;
 
+	float len = 50;
+
 	void Start () {
-		circle = transform.FindChild ("Circle");
-		originPos = circle.localPosition;
+		originPos = transform.localPosition;
 
-		UIEventListener.Get (circle.gameObject).OnPointerDown = onDown;
-		UIEventListener.Get (circle.gameObject).onDrag = onDrag;
-		UIEventListener.Get (circle.gameObject).OnPointerDown = onUp;
+		UIEventListener.Get (gameObject).onDown = OnDown;
+		UIEventListener.Get (gameObject).onDrag = OnDrag;
+		UIEventListener.Get (gameObject).onUp = OnUp;
+		Tools.Log ("origin:" + originPos.x + "  " + originPos.y);
 	}
 
 
-	void onDown(GameObject go, PointerEventData eventData)
+	void OnDown(GameObject go, PointerEventData eventData)
 	{
-		
+		Vector2 pos = eventData.position;
+		Vector3 nowPos = new Vector3 (pos.x, pos.y);
+		transform.position = nowPos;
+		transform.localPosition = transform.localPosition.normalized * len;
 	}
 
 
-	void onDrag(GameObject go, PointerEventData eventData)
+	void OnDrag(GameObject go, PointerEventData eventData)
 	{
+		Vector2 pos = eventData.delta;
+		Vector3 nowPos = new Vector3 (pos.x, pos.y);
+		transform.localPosition = transform.localPosition + nowPos;
+		if (transform.localPosition.magnitude > len) {
+			transform.localPosition = transform.localPosition.normalized * len;
+		}
 
+		Tools.Log ("origin:" + transform.localPosition.x + "  " + transform.localPosition.y);
 	}
 
 
-	void onUp(GameObject go, PointerEventData eventData)
+	void OnUp(GameObject go, PointerEventData eventData)
 	{
-
+//		Tools.Log ("origin:" + originPos.x + "  " + originPos.y);
+		transform.localPosition = originPos;
 	}
 }
